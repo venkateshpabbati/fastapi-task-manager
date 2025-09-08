deaa41925382a84ee73e13036f9df4db2c03bc25
VenkateshPabbati <venkateshpabbati@gmail.com>
2025-09-08 10:47:17 +0530

    Add crud.py

---
+++ crud.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+def get_task(db: Session, task_id: int):
+    return db.query(models.Task).filter(models.Task.id == task_id).first()
+
+def get_tasks(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Task).offset(skip).limit(limit).all()
+
+def create_task(db: Session, task: schemas.TaskCreate):
+    db_task = models.Task(**task.dict())
+    db.add(db_task)
+    db.commit()
+    db.refresh(db_task)
+    return db_task
+
+def update_task(db: Session, task_id: int, task: schemas.TaskCreate):
+    db_task = get_task(db, task_id)
+    if not db_task:
+        return None
+    for field, value in task.dict().items():
+        setattr(db_task, field, value)
+    db.commit()
+    db.refresh(db_task)
+    return db_task
+
+def delete_task(db: Session, task_id: int):
+    db_task = get_task(db, task_id)
+    if not db_task:
+        return None
+    db.delete(db_task)
+    db.commit()
+    return db_task
